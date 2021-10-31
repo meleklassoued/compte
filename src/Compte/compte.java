7dbe7c153171df91ff6fff4aab1e0a684f7ef074
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-10-31 13:43:14 +0100

    ajouter touts les methodes de class compte

--- src/Compte/compte.java
+++ src/Compte/compte.java
@@ -5,19 +5,51 @@ public class compte {
     private String nom;
     private String prénom;
 
-    public compte() {}
-
-    public void setSolde(float solde){
-        this.solde ;
-        
+    public compte() {
     }
-    
+/* ------------------------ getter et setter de solde ----------------------- */
+    public void setSolde(float solde) {
+        this.solde = solde;
+    }
+    public float getSolde() {
+        return this.solde;
+    }
+    /* ---------------------- fin setter getter pour solde ---------------------- */
+
+
+
+    /* ------------------------ getter et setter de Nom ----------------------- */
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getNom() {
+        return this.nom;
+    }
+    /* ---------------------- fin setter getter pour Nom---------------------- */
+
+
+
+
+    /* ------------------------ getter et setter de preNom ----------------------- */
+
+    public void setPrénom(String prénom) {
+        this.prénom = prénom;
+    }
+
+    public String getPrénom() {
+        return this.prénom;
+    }
+    /* ---------------------- fin setter getter pour preNom---------------------- */
+
+
+
+
+
+
     public void desposer(float solde) {
         this.solde += solde;
     }
-
-
-    
 
     public void retirer(float solde) {
         this.solde -= solde;
