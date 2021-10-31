d035d475dbaaa7543c00cca37e2348a59fbc0bf9
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-10-31 17:51:37 +0100

    finish the exercice

--- src/App.java
+++ src/App.java
@@ -1,8 +1,39 @@
 import Compte.compte;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        compte C = new compte(500, "melek", "lassoued");
-        
+        Scanner clavier = new Scanner(System.in);
+        System.out.println("donner la taille du tableau des compte ");
+        int taille = clavier.nextInt();
+        compte[] tab = new compte[taille];
+
+        // ! remplissage du tableau :
+
+        for (int i = 0; i < tab.length; i++) {
+            tab[i] = new compte();
+            int indiceTab = i + 1;
+            System.out.println("le compte " + indiceTab);
+            System.out.println("donner le nom ");
+            System.out.println("donner le prenom ");
+            String nom = clavier.nextLine();
+            String Prenom = clavier.nextLine();
+            tab[i].setNom(nom);
+            tab[i].setPrénom(Prenom);
+
+            tab[i].desposer(100 + i);
+
+        }
+
+        for (int i = 0; i < tab.length - 1; i++) {
+
+            int trans = 10;
+            tab[i].transfert(trans, tab[i + 1]);
+
+        }
+        for (int i = 0; i < tab.length; i++) {
+            tab[i].afficher();
+        }
+
     }
 }
